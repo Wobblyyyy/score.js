dbf27ad08d951c8856e50f7803a66b91ca189a9d
Wobblyyyy <30844830+Wobblyyyy@users.noreply.github.com>
2020-05-14 21:49:40 -0400

    Remove the useless _T property, add an error message when query yields no results

--- src/_.js
+++ src/_.js
@@ -19,13 +19,23 @@ const _ = function (selector)
   return $E;
 }
 
+/**
+ * Internal function with all of the prototypes we use to make score.js
+ * function as it does.
+ * @param selector the selector which it should use
+ * @constructor
+ */
 const E = function (selector)
 {
   this._S = selector || null;
   this._E = null;
-  this._T = null;
 }
 
+/**
+ * Select different types of elements, including by ID, by class name, by
+ * SV, as body, and more.
+ * @constructor
+ */
 E.prototype.I = function ()
 {
   const selectorType = this._S.charAt(0);
@@ -33,11 +43,9 @@ E.prototype.I = function ()
   {
     case ".":
       this._E = document.getElementsByClassName(this._S.substr(1));
-      this._T = "class";
       break;
     case "#":
       this._E = document.getElementById(this._S.substr(1));
-      this._T = "id";
       break;
     case "$":
       this._E = document.querySelector(('[sv="' + this._S.substr(1) + '"]'));
@@ -45,10 +53,11 @@ E.prototype.I = function ()
       break;
     case "@":
       this._E = document.body;
-      this._T = "body";
       break;
     default:
       this._E = document[this._S];
       break;
   }
+  if (this._E === U)
+    console.error("Error in score.js/_.js: No element(s) could be found matching the query for \"" + this._S +  "\"");
 }
