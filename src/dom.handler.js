3784e165812fc8603bb6689f5eed990985d7a1ea
Wobblyyyy <30844830+Wobblyyyy@users.noreply.github.com>
2020-05-15 20:01:29 -0400

    Fix event unbinding and add more documentation.

--- src/dom.handler.js
+++ src/dom.handler.js
@@ -61,7 +61,18 @@ E.prototype.off = function (event)
  * @type {{bind: E.eventBinderObject.bind, unbind: E.eventBinderObject.unbind, fetch: (function(*=, *): *), events: []}}
  */
 E.prototype.eventBinderObject = {
+
+  /**
+   * An array list of all of the events we have to keep track of.
+   */
   events: [],
+
+  /**
+   * Bind an event.
+   * @param event the event name to bind (click, value, etc)
+   * @param element the element to bind the event to
+   * @param callback the callback function that should be ran
+   */
   bind: function (event, element, callback)
   {
     this.unbind(event, element);
@@ -72,18 +83,34 @@ E.prototype.eventBinderObject = {
       'eventElement': element
     });
   },
+
+  /**
+   * Sort through the array of events and find the one matching the
+   * requested parameters (event & element)
+   * @param event the event to search for
+   * @param element the element to search for
+   * @returns {*} returns the (hopefully) searched for event information
+   */
   fetch: function (event, element)
   {
-    return this.events.filter(function (event, element)
+    return this.events.filter(function (el)
     {
-      return (event.eventType === event) && (event.eventElement === element);
+      return (event === el.eventType) && (element === el.eventElement);
     }, event)[0];
   },
+
+  /**
+   * Checks to see if the event which is going to be unbound is in the list
+   * of currently registered events. If it is, unbind it, and if it's not,
+   * don't do anything at all.
+   * @param event the event to search for
+   * @param element the element to unbind from and search for
+   */
   unbind: function (event, element)
   {
     if (this.fetch(event, element) !== U)
     {
-      element.removeEventListener(event, this.fetch(event, element).eventType);
+      element.removeEventListener(event, this.fetch(event, element).eventCallback);
     }
   }
 }
